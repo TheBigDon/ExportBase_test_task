07c06cfeee9e221310051e26265182968e211379
Stepan <takistepan@gmail.com>
2023-04-18 11:58:32 +0300

    добавлено отображение изобображений с комментариями на странице

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -1,1 +1,38 @@
-<template>HomePage!</template>
+<script>
+import ImageItem from "../components/ImageItem.vue";
+import { getImages } from "../services/images";
+export default {
+  data() {
+    return {
+      images: [],
+    };
+  },
+  async mounted() {
+    this.images = await getImages();
+  },
+  components: { ImageItem },
+  methods: {
+    deleteComment(id, imageId) {
+      const imageIndex = this.images.findIndex((image) => image.id === imageId);
+      if (imageIndex === -1) {
+        return;
+      }
+      this.images[imageIndex] = this.images[imageIndex].comments.filter(
+        (image) => {
+          return image.id !== id;
+        }
+      );
+    },
+  },
+};
+</script>
+<template>
+  <div>
+    <ImageItem
+      v-for="image in images"
+      :image="image"
+      :key="image.id"
+      @delete="deleteComment"
+    />
+  </div>
+</template>
