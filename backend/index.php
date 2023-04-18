fd48aadb2c56c7cddfe9ce2ffee83752617d82e8
Stepan <takistepan@gmail.com>
2023-04-18 12:01:17 +0300

    исправлен импорт файлов, изменено создание контроллеров

--- backend/index.php
+++ backend/index.php
@@ -1,7 +1,8 @@
 <?php
 
-use controller\CommentController;
-use database\DatabaseConnector;
+include('controller\CommentController.php');
+include('controller\ImageController.php');
+include('database\DatabaseConnector.php');
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -10,23 +11,20 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode( '/', $uri );
-
-if ($uri[1] !== 'comment') {
-    header("HTTP/1.1 404 Not Found");
-    exit();
-}
-
-$commentId = null;
-if (isset($uri[2])){
-    $commentId = (int) $uri[2];
-}
+$uri = explode('/', $uri);
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
 $dbConnection = (new DatabaseConnector())->getConnection();
 
-$controller = new CommentController($dbConnection, $requestMethod, $commentId);
-$controller->processRequest();
+if ($uri[1] == 'comments') {
+    $controller = new CommentController($dbConnection, $requestMethod);
+    $controller->processRequest();
+} else if ($uri[1] == 'images') {
+    $controller = new ImageController($dbConnection, $requestMethod);
+    $controller->processRequest();
+} else {
+    header("HTTP/1.1 404 Not Found");
+}
 
 ?>
