a65018361b5c0ff4569b8e2228cc24e091332e78
Stepan <takistepan@gmail.com>
2023-04-18 12:06:30 +0300

    функция получения изображения по его id заменена на функцию получения всех изображений с их комментариями

--- backend/model/Image.php
+++ backend/model/Image.php
@@ -1,6 +1,7 @@
 <?php
 
-class Image {
+class Image
+{
 
     private $db = null;
 
@@ -9,17 +10,55 @@ class Image {
         $this->db = $db;
     }
 
-    public function find($id)
+    public function findAll()
     {
-        $statement = "SELECT * FROM content WHERE id = ?;";
+        $statement = "SELECT * 
+                      FROM content 
+                      JOIN comments on content.id = comments.id_image 
+                      ORDER BY comments.date_comment DESC;";
 
         try {
             $statement = $this->db->prepare($statement);
-            $statement->execute(array($id));
+            $statement->execute();
             $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-            return $result;
+            $result = json_encode($result);
+            $result = json_decode($result);
+            $images = array();
+            foreach ($result as $image) {
+                if (array_search($image->id_image, array_column($images, 'id')) === false) {
+                    array_push(
+                        $images,
+                        array(
+                            'id' => $image->id_image,
+                            'image_url' => $image->image_url,
+                            'comments' => array(
+                                array(
+                                    'id' => $image->id,
+                                    'user' => $image->user,
+                                    'text_comment' => $image->text_comment,
+                                    'date_comment' => $image->date_comment
+                                )
+                            )
+                        )
+                    );
+                } else {
+                    $key = array_search($image->id_image, array_column($images, 'id'));
+                    $comments = $images[$key]['comments'];
+                    array_push(
+                        $comments,
+                        array(
+                            'id' => $image->id,
+                            'user' => $image->user,
+                            'text_comment' => $image->text_comment,
+                            'date_comment' => $image->date_comment
+                        )
+                    );
+                    $images[$key]['comments'] = $comments;
+                }
+            }
+            return $images;
         } catch (PDOException $e) {
-            exit($e->getMessage());
+            echo 'Error: ' . $e->getMessage();
         }
     }
 }
