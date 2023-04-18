a271ef3c80b9c20d84d65ef8ee2e12d611111170
Stepan <takistepan@gmail.com>
2023-04-18 11:51:34 +0300

    реализован компонент изображений

---
+++ frontend/src/components/ImageItem.vue
@@ -0,0 +1,48 @@
+<script>
+import ImageComment from "./ImageComment.vue";
+
+export default {
+  props: {
+    image: {
+      type: Object,
+      required: true,
+    },
+  },
+  components: { ImageComment },
+  methods: {
+    deleteHandler(id) {
+      this.$emit("delete", id, this.image.id);
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="post">
+    <img class="post__image" :src="image.image_url" />
+    <div class="post__comments">
+      <ImageComment
+        v-for="comment in image.comments"
+        :comment="comment"
+        :key="comment.id"
+        @delete="deleteHandler"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.post {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+.post__comments {
+  width: 600px;
+}
+
+.post__image {
+  display: block;
+}
+</style>
