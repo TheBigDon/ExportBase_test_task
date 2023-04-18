30e079e24f9343ba2bd4d802a5e2ff6682aa675e
Stepan <takistepan@gmail.com>
2023-04-18 12:04:39 +0300

    удалена функция получения всех комментариев изображения по его id

--- backend/model/Comment.php
+++ backend/model/Comment.php
@@ -1,6 +1,7 @@
 <?php
 
-class Comment {
+class Comment
+{
 
     private $db = null;
 
@@ -9,39 +10,27 @@ class Comment {
         $this->db = $db;
     }
 
-    public function findByImageId($id)
-    {
-        $statement = "SELECT * FROM comments WHERE id_image = ?;";
-
-        try {
-            $statement = $this->db->prepare($statement);
-            $statement->execute(array($id));
-            $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-            return $result;
-        } catch (PDOException $e) {
-            exit($e->getMessage());
-        }
-    }
-
-    public function insert(Array $input) 
+    public function insert(array $input)
     {
         $statement = "INSERT INTO comments(user, text_comment, date_comment, id_image)
                       VALUES(:user, :text_comment, now(), :id_image);";
 
         try {
             $statement = $this->db->prepare($statement);
-            $statement->execute(array(
-                'user' => $input['user'],
-                'text_comment' => $input['text_comment'],
-                'id_image' => $input['id_image'],
-            ));
+            $statement->execute(
+                array(
+                    'user' => $input['user'],
+                    'text_comment' => $input['text_comment'],
+                    'id_image' => $input['id_image'],
+                )
+            );
             return $statement->rowCount();
-        } catch (PDOException $e){
-            exit($e->getMessage());
+        } catch (PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
         }
     }
 
-    public function delete($id) 
+    public function delete($id)
     {
         $statement = "DELETE FROM comments WHERE id = :id;";
 
@@ -49,8 +38,8 @@ class Comment {
             $statement = $this->db->prepare($statement);
             $statement->execute(array('id' => $id));
             return $statement->rowCount();
-        } catch (PDOException $e){
-            exit($e->getMessage());
+        } catch (PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
         }
     }
 }
