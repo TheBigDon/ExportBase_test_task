c508966aed12140ff8229b58284147565280c52b
Stepan <takistepan@gmail.com>
2023-04-18 11:51:02 +0300

    реализован компонент комментариев

---
+++ frontend/src/components/ImageComment.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  props: {
+    comment: {
+      type: Object,
+      required: true,
+    },
+  },
+  emits: ["delete"],
+  methods: {
+    deleteHandler() {
+      this.$emit("delete", this.comment.id);
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="comment">
+    <div>
+      <div>
+        <span>{{ comment.user }}</span>
+        <span>{{ comment.date_comment }}</span>
+      </div>
+      <p>{{ comment.text_comment }}</p>
+    </div>
+    <button @click="deleteHandler">Удалить</button>
+  </div>
+</template>
+
+<style scoped>
+.comment {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
